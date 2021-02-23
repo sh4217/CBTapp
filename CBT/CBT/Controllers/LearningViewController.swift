9b9f2c7a8d282ffca8a963a6eaad344bca469861
Sean Hall <seanhall4217@gmail.com>
2021-02-23 12:22:14 -0500

    linked second VC to assistant editor

--- CBT/CBT/Controllers/LearningViewController.swift
+++ CBT/CBT/Controllers/LearningViewController.swift
@@ -11,7 +11,8 @@ class LearningViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+      
         // Do any additional setup after loading the view.
     }
     
