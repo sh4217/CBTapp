4b4344c973dce4747d6eb4ffb73df7033c8d73ff
Sean Hall <seanhall4217@gmail.com>
2021-02-28 11:15:21 -0500

    added return button to learning screen

--- CBT/CBT/Controllers/LearningViewController.swift
+++ CBT/CBT/Controllers/LearningViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class LearningViewController: UIViewController {
 
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
