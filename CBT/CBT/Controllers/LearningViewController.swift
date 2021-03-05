4bcc0dcd2d51377f91057b653303265ee053e9cc
Sean Hall <seanhall4217@gmail.com>
2021-03-04 19:22:01 -0500

    made one common IBAction for the twelve different buttons on the learning screen

--- CBT/CBT/Controllers/LearningViewController.swift
+++ CBT/CBT/Controllers/LearningViewController.swift
@@ -10,19 +10,7 @@ import UIKit
 class LearningViewController: UIViewController {
 
     var textLabel: String = ""
-    
-    @IBOutlet weak var basicPrinciplesLabel: UILabel!
-    @IBOutlet weak var allOrNothingThinkingLabel: UILabel!
-    @IBOutlet weak var mentalFilterLabel: UILabel!
-    @IBOutlet weak var disqualifyingLabel: UILabel!
-    @IBOutlet weak var jumpingLabel: UILabel!
-    @IBOutlet weak var magnificationLabel: UILabel!
-    @IBOutlet weak var emotionalReasoningLabel: UILabel!
-    @IBOutlet weak var shouldStatementsLabel: UILabel!
-    @IBOutlet weak var labelingLabel: UILabel!
-    @IBOutlet weak var generalAdviceLabel: UILabel!
-    
-    
+
     @IBAction func close() {
         dismiss(animated: true, completion: nil)
     }
@@ -31,85 +19,12 @@ class LearningViewController: UIViewController {
         super.viewDidLoad()
         
     }
-    
-    /*
-     Each of these buttons performs a basic segue to the Information screen,
-     where the relevant information will be displayed. Unfortunately, it is
-     not possible to write one IBAction for each button to inherit, because
-     the segue takes the text data from a label that is superimposed on the
-     button but is not a part of the button. The text label attribute of the
-     button itself is not sufficient because it lacks formatting options.
-    */
-    @IBAction func basicPrinciplesButtonPressed(_ sender: UIButton) {
-        textLabel = basicPrinciplesLabel.text!
+
+    @IBAction func categoryNameButtonPressed(_ sender: UIButton) {
+        textLabel = sender.currentTitle!
         performSegue(withIdentifier: "goToInformation", sender: self)
     }
-    
-    @IBAction func allOrNothingButtonPressed(_ sender: UIButton) {
-        textLabel = allOrNothingThinkingLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    /*
-     The label on the button contains a hyphen due to width restrictions.
-     However, there is enough width on the Information screen to display
-     the entire word, so this is the one example where textLabel must
-     be specified as opposed to getting the data from a UI Label.
-     */
-    @IBAction func overgeneralizationButtonPressed(_ sender: Any) {
-        textLabel = "Overgeneralization"
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func mentalFilterButtonPressed(_ sender: UIButton) {
-        textLabel = mentalFilterLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func disqualifyingButtonPressed(_ sender: Any) {
-        textLabel = disqualifyingLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func jumpingButtonPressed(_ sender: UIButton) {
-        textLabel = jumpingLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func magnificationButtonPressed(_ sender: UIButton) {
-        textLabel = magnificationLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func emotionalReasoningButtonPressed(_ sender: UIButton) {
-        textLabel = emotionalReasoningLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func shouldStatementsButtonPressed(_ sender: UIButton) {
-        textLabel = shouldStatementsLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    @IBAction func labelingButtonPressed(_ sender: UIButton) {
-        textLabel = labelingLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    /*
-     Same width issue here as with overgeneralization.
-     */
-    @IBAction func personalizationButtonPressed(_ sender: UIButton) {
-        textLabel = "Personalization"
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
-    
-    @IBAction func generalAdviceButtonPressed(_ sender: UIButton) {
-        textLabel = generalAdviceLabel.text!
-        performSegue(withIdentifier: "goToInformation", sender: self)
-    }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToInformation" {
             let informationVC = segue.destination as! InformationViewController
