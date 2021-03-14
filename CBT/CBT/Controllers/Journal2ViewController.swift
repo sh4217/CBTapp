94136621d1321dc86397a5014eca9e5c50d2485c
Sean Hall <seanhall4217@gmail.com>
2021-03-13 21:33:49 -0500

    added second journal view controller where you can select which cognitive distortion applies best to your entry

---
+++ CBT/CBT/Controllers/Journal2ViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class Journal2ViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
