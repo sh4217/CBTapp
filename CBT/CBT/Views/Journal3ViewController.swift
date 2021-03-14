dd441e666ab7ee677208d4c22b60c710ece31abc
Sean Hall <seanhall4217@gmail.com>
2021-03-14 13:32:23 -0400

    added text view for journal entry to journal view 3

--- CBT/CBT/Views/Journal3ViewController.swift
+++ CBT/CBT/Views/Journal3ViewController.swift
@@ -2,7 +2,8 @@ import UIKit
 
 class Journal3ViewController: UIViewController {
 
-    @IBOutlet weak var exampleTextBox: UILabel!
+    
+    @IBOutlet weak var textBox: UILabel!
     var distortionName = ""
     
     @IBAction func close() {
@@ -11,7 +12,8 @@ class Journal3ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        exampleTextBox.text = distortionName
+        textBox.text =
+        "You have taken a crucial step by recognizing that your negative emotions may be emanating from thoughts suffering from the following cognitive distortion: \(distortionName).\n\nNow, reframe your thoughts in a healthier way that is not constrained by this distortion."
     }
 
 }
